09ca15165afd2430ebae1d489791cf8a54e52d76
Khrystyna_Salo <Khrystyna_Salo@epam.com>
2018-05-08 14:18:25 +0300

    angular added

--- src/main/frontend/src/app/content/chart/chart.component.ts
+++ src/main/frontend/src/app/content/chart/chart.component.ts
@@ -1,56 +1,66 @@
-import { Input, Component } from '@angular/core';
+import {Input, Component, OnInit, OnChanges} from '@angular/core';
 import { Chart } from 'angular-highcharts';
-import { chartData } from "../../shared/models/chartData";
+import { chartData } from '../../shared/models/chartData';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit, OnChanges {
 
   @Input() chartConfig: chartData[];
+  chart: Chart;
 
-  chart = new Chart({
-    chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: null,
-      plotShadow: false,
-      type: 'pie',
-      width: 300,
-      height: 300,
-      backgroundColor: 'transparent'
-    },
-    legend: {
-      align: 'right',
-      verticalAlign: 'top',
-      layout: 'vertical',
-      x: 0,
-      y: 100
-    },
-    title: {
-      text: 'Customer Satisfaction in January, 2018'
-    },
-    credits: {
-      enabled: false
-    },
-    tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-          enabled: false
-        },
-        showInLegend: true,
-        size: 150
-      }
-    },
-    series: [{
-      name: 'Brands',
-      data: this.chartConfig
-    }]
-  });
+  ngOnChanges(): void {
+    this.chart = new Chart({
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie',
+        width: 300,
+        height: 300,
+        backgroundColor: 'transparent'
+      },
+      legend: {
+        align: 'right',
+        verticalAlign: 'top',
+        layout: 'vertical',
+        x: 0,
+        y: 100
+      },
+      title: {
+        text: 'Customer Satisfaction in January, 2018'
+      },
+      credits: {
+        enabled: false
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: false
+          },
+          showInLegend: true,
+          size: 150
+        }
+      },
+      series: [{
+        name: 'Brands',
+        data: this.chartConfig
+      }]
+    });
+  }
+
+  ngOnInit(): void {
+    console.log(this.chartConfig);
+
+  }
+
+
 }
--- src/main/frontend/src/app/content/content.component.ts
+++ src/main/frontend/src/app/content/content.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Brand} from "../shared/models/brand";
-import {chartData} from "../shared/models/chartData";
-import {HttpService} from "../shared/content.service";
+import {Brand} from '../shared/models/brand';
+import {chartData} from '../shared/models/chartData';
+import {HttpService} from '../shared/content.service';
 
 @Component({
   selector: 'app-content',
@@ -9,52 +9,29 @@ import {HttpService} from "../shared/content.service";
   styleUrls: ['./content.component.scss']
 })
 export class ContentComponent implements OnInit {
-  brands: Brand[]=[];
-  filteredBrands: Brand[]=[];
-  chartDataList: chartData[]=[];
-  test: {};
+  brands: Brand[] = [];
+  filteredBrands: Brand[] =[ ];
+  chartDataList: chartData[] = [];
+  sortValue: boolean = false;
 
   constructor(private httpService: HttpService){}
 
   ngOnInit(){
     this.httpService.getData().subscribe(data => {
-      this.brands=data["brandsList"];
-      this.filteredBrands=data["brandsList"];
-      console.log(data['brandsList']);
-      this.chartDataList = [
-        {
-          name: 'Nike',
-          y: 61.41
-        },
-        {
-          name: 'Adidas',
-          y: 11.84
-        },
-        {
-          name: 'Converse',
-          y: 10.85
-        },
-        {
-          name: 'Reebok',
-          y: 4.18
-        },
-        {
-          name: 'Puma',
-          y: 7.05
-        },
-        {
-          name: 'New Balance',
-          y: 4.67
-        },
-      ]
+      this.brands=data['brandsList'];
+      this.filteredBrands=data['brandsList'];
+      this.chartDataList = [];
       data["brandsList"].forEach((elem) => {
-
+        let obj = {name: '', y: null};
+        obj.name = elem.name;
+        obj.y = elem.amount;
+        this.chartDataList.push(obj)
       })
     });
   }
 
   search(domElem) {
-    const inputValue =  domElem.value.toLowerCase();
+    const inputValue =  domElem.value.toLowerCase().trim();
     this.filteredBrands = this.brands.filter((elem) => {
       return elem.name.toLowerCase().indexOf(inputValue) >= 0
     });
@@ -66,7 +43,15 @@ export class ContentComponent implements OnInit {
   }
 
   sort() {
-    this.filteredBrands.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+    this.sortValue = !this.sortValue;
+    if(this.sortValue) {
+      this.filteredBrands.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0);
+    }
+
+    else {
+      this.filteredBrands.sort((a, b) => a.name < b.name ? 1 : a.name > b.name ? -1 : 0);
+    }
   }
+
 
 }
